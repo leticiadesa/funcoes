086b647580ef68dcd1c64c61594fb1803de74e4c
leticiadesa <leticiadesa578@gmail.com>
2026-03-17 23:21:42 -0300

    Update ConcatenacaoString1.js

--- funções/Concatenação String/ConcatenacaoString1.js
+++ funções/Concatenação String/ConcatenacaoString1.js
@@ -8,14 +8,3 @@ function escreverBomDia() {
 }
 
 export { escreverBomDia }
-
-function juntarNome() {
-    let nome1, nome2
-
-    nome1 = 'Miguel'
-    nome2 = ' Angelo'
-
-    return nome1 + nome2
-}
-
-export { juntarNome }
