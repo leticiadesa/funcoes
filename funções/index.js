72bb1ca720020831f0184138be1f66cffaa830eb
leticiadesa <leticiadesa578@gmail.com>
2026-03-24 19:31:54 -0300

    Update index.js

--- funções/index.js
+++ funções/index.js
@@ -1,16 +1,21 @@
+
+// Strings
 import { escreverBomDia } from './Concatenação String/ConcatenacaoString1.js'
 import { juntarNome } from './Concatenação String/ConcatenacaoString2.js'
 
+// Variáveis
 import { declararLet } from './Declaração de Variaveis/DeclaracaoVariaveisLet.js'
 import { declararVar } from './Declaração de Variaveis/DeclaracaoVariaveisVar.js'
 import { declararConst } from './Declaração de Variaveis/DeclaraçaoVariaveisConst.js'
 
+// Condicionais
 import { verificarNumero } from './Estrutura Condicional/IfComposto.js'
 import { verificarAprovacao } from './Estrutura Condicional/IfSimples.js'
 import { verificarParOuImpar } from './Estrutura Condicional/IfTradicional.js'
 import { verificarDiaSemana } from './Estrutura Condicional/SwitchCase.js'
 import { verificarResultado } from './Estrutura Condicional/Ternario.js'
 
+// Laços
 import { mostrarPares } from './Laço de Repetição/DoWhile.js'
 import { mostrarNumeros } from './Laço de Repetição/DoWhile2.js'
 import { mostrarTabuada } from './Laço de Repetição/For.js'
@@ -19,61 +24,59 @@ import { mostrarDobro } from './Laço de Repetição/Map.js'
 import { mostrarHelloWorld } from './Laço de Repetição/While.js'
 import { mostrarTabuada1 } from './Laço de Repetição/While2.js'
 
+// Aritméticos
 import { somar, subtrair, multiplicar, dividir, potencia, raizQuadrada, restoDivisao } from './Operadores Aritméticos/operadoresAritmeticos.js'
 
+// Lógicos
 import { verificarE } from './Operadores Lógicos/OpLogicoE.js'
 import { verificarNao } from './Operadores Lógicos/OpLogicoNot.js'
 import { verificarOu } from './Operadores Lógicos/OpLogicoOu.js'
 
-import { diferente } from './OperadoresRelacionais/OpRelacionaisDiferente.js'
-import { igual } from './OperadoresRelacionais/OpRelacionaisIgual.js'
-import { maior } from './OperadoresRelacionais/OpRelacionaisMaior.js'
-import { maiorOuIgual } from './OperadoresRelacionais/OpRelacionaisMaiorIgual.js'
-import { menor } from './OperadoresRelacionais/OpRelacionaisMenor.js'
-import { menorOuIgual } from './OperadoresRelacionais/OpRelacionaisMenorIgual.js'
+// Relacionais
+import { diferente, igual, maior, maiorOuIgual, menor, menorOuIgual } from './OperadoresRelacionais/index.js'
 
-import { acumulacao } from './Vetores/VetorAcumulacao.js'
-import { adicionarElemento } from './Vetores/VetorAdicao.js'
-import { buscarElemento } from './Vetores/VetorBusca.js'
-import { removerElemento } from './Vetores/VetorRemocao.js'
+// Vetores
+import { acumulacao, adicionarElemento, buscarElemento, removerElemento } from './Vetores/index.js'
 
-console.log('----------------')
-console.log('Concatenacao de string')
-console.log('----------------')
+
+function titulo(nome) {
+    console.log('\n----------------')
+    console.log(nome)
+    console.log('----------------')
+}
+
+
+// Strings
+titulo('Concatenação de String')
 console.log(escreverBomDia())
 console.log(juntarNome())
 
-console.log('----------------')
-console.log('Declaracao de variaveis')
-console.log('----------------')
+// Variáveis
+titulo('Declaração de Variáveis')
 console.log(declararLet())
 console.log(declararVar())
 console.log(declararConst())
 
-console.log('----------------')
-console.log('Estrutura Condicional')
-console.log('----------------')
+// Condicionais
+titulo('Estrutura Condicional')
 console.log(verificarNumero(10))
 console.log(verificarAprovacao(2))
 console.log(verificarParOuImpar(5))
 console.log(verificarDiaSemana(1))
 console.log(verificarResultado(1))
 
-console.log('----------------')
-console.log('Laço de Repetição')
-console.log('----------------')
+// Laços
+titulo('Laço de Repetição')
 console.log(mostrarPares())
 console.log(mostrarNumeros())
-console.log(mostrarTabuada(10))
 console.log(mostrarTabuada(10))
 console.log(mostrarNumerosVetor())
 console.log(mostrarDobro())
 console.log(mostrarHelloWorld())
 console.log(mostrarTabuada1())
 
-console.log('----------------')
-console.log('Operadores Aritméticos')
-console.log('----------------')
+// Aritméticos
+titulo('Operadores Aritméticos')
 console.log(somar(5, 3))
 console.log(subtrair(10, 4))
 console.log(multiplicar(2, 6))
@@ -82,30 +85,24 @@ console.log(potencia(2, 3))
 console.log(raizQuadrada(16))
 console.log(restoDivisao(10, 3))
 
-console.log('----------------')
-console.log('Operadores Logicos')
-console.log('----------------')
+// Lógicos
+titulo('Operadores Lógicos')
 console.log(verificarE(true, false))
 console.log(verificarOu(true, false))
 console.log(verificarNao(true))
 
-console.log('----------------')
-console.log('Operadores Relacionais')
-console.log('----------------')
-console.log(diferente(5,3))
-console.log(igual(4,4))
-console.log(maior(10,5))
-console.log(maiorOuIgual(5,5))
-console.log(menorOuIgual(3,8))
-console.log(menor(2,9))
+// Relacionais
+titulo('Operadores Relacionais')
+console.log(diferente(5, 3))
+console.log(igual(4, 4))
+console.log(maior(10, 5))
+console.log(maiorOuIgual(5, 5))
+console.log(menorOuIgual(3, 8))
+console.log(menor(2, 9))
 
-
-console.log('----------------')
-console.log('Vetores')
-console.log('----------------')
+// Vetores
+titulo('Vetores')
 console.log(acumulacao())
 console.log(adicionarElemento())
 console.log(buscarElemento())
 console.log(removerElemento())
-
-
