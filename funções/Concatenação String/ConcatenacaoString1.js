a3e37426df503ca2989ef386515248b7d644c0d7
leticiadesa <leticiadesa578@gmail.com>
2026-03-24 22:16:00 -0300

    Update ConcatenacaoString1.js

--- funções/Concatenação String/ConcatenacaoString1.js
+++ funções/Concatenação String/ConcatenacaoString1.js
@@ -8,3 +8,14 @@ function escreverBomDia() {
 }
 
 export { escreverBomDia }
+
+function juntarNome() {
+    let nome1, nome2
+
+    nome1 = 'Letícia'
+    nome2 = ' de Sá'
+
+    return nome1 + nome2
+}
+
+export { juntarNome }
